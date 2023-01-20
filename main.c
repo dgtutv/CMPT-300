efc640600b3bacfbc97d3a9c4186109b1bf0ae01
Daniel <dgtutv@gmail.com>
2023-01-20 15:31:39 -0800

    Implementation of FREE_FN from list.c

--- main.c
+++ main.c
@@ -2,9 +2,14 @@
 #include <stdio.h>
 #include <assert.h>
 
+
+//My implementation of FREE_FN from list.c
+void freeItem(void* item){
+    return;     //I will test with static memory for simplicity
+}
+
 int main(){
     extern Manager manager; //our manager from list.c
-
     //Tests for List_create()
     List* newList = List_create();
     List* nullList = 0;
