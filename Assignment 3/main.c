9dcddf0d005d24bae54560be274bb08db489731b
Daniel <dgtutv@gmail.com>
2023-03-09 11:04:27 -0800

    Added extra information for when processes created, all processes can now be accessed by a List consisting of all processes, no matter the state

--- Assignment 3/main.c
+++ Assignment 3/main.c
@@ -12,6 +12,7 @@ List* highQueue;    //The ready queue for high priority processes
 List* sendQueue;    //The queue of processes waiting on a send operation (i.e., waiting for a reply)
 List* recvQueue;    //The queue of processes waiting on a receive operation
 List* blockedQueue; //The queue of all blocked processes for our operating system
+List* processes;    //The list of all of our processes
 
 
 //---------------------------------------------OS data structures-------------------------------------------//
@@ -53,6 +54,7 @@ struct PCB* create(int priority){
     else{
         List_prepend(highQueue, process);
     }
+    List_append(processes, process);
     return(process);
 }
 
@@ -79,7 +81,7 @@ void commands(char input){
             else if(newProcess->priority == 2){
                 priority="low";
             }
-            printf("Created a new process with ID %d with %s priority\n", newProcess->ID, priority);
+            printf("Created a new process with ID %d and it added to the %s priority ready queue\n", newProcess->ID, priority);
             return;
         }
         return;
@@ -98,12 +100,15 @@ int main(int argc, char* argv[]){
     sendQueue = List_create();    
     recvQueue = List_create();    
     blockedQueue = List_create(); 
+    processes = List_create();
 
     //Create our init process
-    struct PCB init;
-    init.priority = high;
-    init.state = running;     //The state should be running when the OS starts execution
-    init.ID=0;
+    struct PCB* init = malloc(sizeof(struct PCB));
+    init->priority = high;
+    init->state = running;     //The state should be running when the OS starts execution
+    init->ID=0;
+    List_append(processes, init);
+    printf("Created the init process with process ID 0, high priority, and state set to running\n");
 
     char buffer;
     while(1){
