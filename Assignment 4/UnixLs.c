8e0877b1119afd46b7a65aeaaff380579b56a2a6
Daniel <dgtutv@gmail.com>
2023-04-05 14:26:32 -0700

    Working on string processing for command-line arguments

--- Assignment 4/UnixLs.c
+++ Assignment 4/UnixLs.c
@@ -7,6 +7,7 @@ Course: CMPT 300 - Operating Systems*/
 #include <stdio.h>
 #include <syscall.h>
 #include "list.h"
+#include <string.h>
 
 /*-------------------------------------------------------------Structs----------------------------------------------------------------------*/
 typedef struct File File;
@@ -41,6 +42,7 @@ struct Directory{
 
 int argumentCount;      //The number of command line arguments supplied
 List* arguments;        //A list of the command line arguments supplied
+List* baseFileNames;     //A list of the names of files or directories, optionally provided by the user at the command-line
 
 //Booleans representing whether or not a certain flag was specified by the user
 bool iFlag;
@@ -50,42 +52,72 @@ bool rFlag;
 /*--------------------------------------------------------------Functions-------------------------------------------------------------------*/
 
 /*Processes a given command-line argument, and sets global booleans iFlag, lFlag, and rFlag accordingly
-Returns 0 on failure, 1 on success*/
-int argumentHandler(char* argument){
-    //TODO: throw an error if a flag is repeated
-    //TODO: throw an error if an invalid flag is entered
+Returns false on failure, true on success*/
+bool argumentHandler(char* argument){
+    //If a hyphen is provided, treat the argument as a set of flags
+    char currentChar;
+    bool hasHyphen = false;
+    for(int i=0; i<strlen(argument); i++){
+        currentChar = argument[i];
+
+        //Set the flags accordingly, ensuring format of input is correct
+        if(currentChar == '-' && !hasHyphen && List_count(baseFileNames) == 0){
+            hasHyphen == true;
+        }
+        if(currentChar == '-' && hasHyphen){
+            printf("UnixLs: invalid format, character \'-\' provided too many times\n");
+            return(false);
+        }
+        if(currentChar == '-' && List_count(baseFileNames) > 0){
+            printf("UnixLs: invalid format, any options must come before file names\n");
+            return(false);
+        }
+        else if(currentChar == 'i' && hasHyphen){
+            iFlag = true;
+        }
+        else if(currentChar == 'R' && hasHyphen){
+            rFlag = true;
+        }
+        else if(currentChar == 'l' && hasHyphen){
+            lFlag = true;
+        }
+    }
+   
+    //If there is no hyphen provided, treat the argument as a file or directory
+    if(!hasHyphen){
+        List_append(baseFileNames, &argument);
+    }
+
+    return(true);
 }
 
 /*----------------------------------------------------------------Main----------------------------------------------------------------------*/
 int main(int argc, char* argv[]){
     //Instanstiate some global variables
     argumentCount = argc;
-    List_create(arguments);
+    arguments = List_create();
+    baseFileNames = List_create();
     iFlag = false;
     lFlag = false;
     rFlag = false;
 
     //Take the flags for ls using command line arguments
-    if(argc > 1 && argc < 4){   
-        char* flags0 = argv[1];
-        List_append(arguments, flags0);
-    }
-    if(argc > 1 && argc < 4){
-        char* flags1 = argv[2];
-        List_append(arguments, flags1);
-    }
-    if(argc > 2 && argc < 4){
-        char* flags2 = argv[3];
-        List_append(arguments, flags2);
-    }
-    if(argc > 4){
-        printf("ERROR: Too many arguments specified, only 3 or less arguments are allowed\n");
-        return(0);
+    for(int i=1; i<argc; i++){
+        List_append(arguments, argv[i]);
     }
 
     //Call argumentHandler for each of our arguments to set the flag booleans accordingly
     if(argc > 1){
-        
+        char* currentArgument = List_first(arguments);
+        bool returnValue;
+        while(currentArgument != NULL){
+            returnValue = argumentHandler(currentArgument);
+            if(returnValue == false){
+                printf("UnixLs: One or more of the provided arguments is invalid\n");
+                return(0);
+            }
+            currentArgument = List_next(arguments);
+        }
     }
 
     //Call functions associated to flags specified by the user in the command-line arguments
